b7277b5398e55777d4ab2a7b92d73943052eafb6
Wallace Sousa <119711762+Wallacessousa@users.noreply.github.com>
2023-03-30 12:28:33 -0400

    Add files via upload

--- mario-game/js/script.js
+++ mario-game/js/script.js
@@ -1,5 +1,45 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+const bgclouds = document.querySelector('.bgclouds');
+const goomba = document.querySelector('.goomba');
+const startBtn = document.querySelector('#startBtn');
+
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
+const milisecondsEl = document.querySelector('#miliseconds');
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let miliseconds = 0;
+let itsPaused = false;
+
+
+
+function startTimer() {
+    interval = setInterval(() =>{
+        if(itsPaused){
+            miliseconds += 10;
+            
+
+            if (miliseconds === 1000) {
+                seconds++;
+                miliseconds = 0;
+            }
+
+            if (seconds === 60) {
+                minutes++;
+                seconds = 0;
+            }
+
+            minutesEl.textContent = minutes;
+            secondsEl.textContent = seconds;
+            milisecondsEl.textContent = miliseconds;
+        }
+    }, 10);
+}
+
 
 const jump = () => {
     mario.classList.add('jump');
@@ -12,6 +52,8 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
+    const bgcloudsPosition = bgclouds.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     console.log(marioPosition);
@@ -20,18 +62,23 @@ const loop = setInterval(() => {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
 
+        clouds.style.animation = "none";
+        clouds.style.left = `${cloudsPosition}px`;
+        
+        bgclouds.style.animation = "none";
+        bgclouds.style.left = `${bgcloudsPosition}px`;
+
         mario.style.animation = "none";
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './images/gameovermario.png';
         mario.style.width = '100px';
-        mario.style.marginLeft = '50px'
+        mario.style.marginLeft = '50px';
 
-        clearInterval(loop); 
-    
-
+        clearInterval(loop);
     }
 
 }, 10);
 
-document.addEventListener('keydown', jump); 
+document.addEventListener('keydown', jump);
+document.addEventListener('click', jump);
